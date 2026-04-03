a1aeaec92f8ba1f558be7aa2f0bc1b123b3d26e4
rameshghule11 <rameshghule112002@gmail.com>
2026-04-03 17:54:35 +0530

    Changes added

--- HttpSessionDemo2/src/main/java/p1/SessionServlet.java
+++ HttpSessionDemo2/src/main/java/p1/SessionServlet.java
@@ -11,51 +11,50 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-
 @WebServlet("/SessionServlet")
 public class SessionServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    
-    public SessionServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	public SessionServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		response.setContentType("text/html");
-		PrintWriter out=response.getWriter();
-		
-		out.print("<h1>" +"Welcome"+"</h1>");
+		PrintWriter out = response.getWriter();
+
+		out.print("<h1>" + "Welcome" + "</h1>");
 		out.print("<hr>");
-		
-		// HOW TO CREATE SESSION 
-		
-		HttpSession session =request.getSession(false);
-		
-		if(session !=null) {
-			
+
+		// HOW TO CREATE SESSION
+		Integer cnt = 0;
+		HttpSession session = request.getSession(false);
+
+		if (session != null) {
+			cnt = (Integer) session.getAttribute("count") + 1;
+			session.setAttribute("count", cnt + 1);
+		} else {
+
+			// session.isNew();
+			session = request.getSession();
+
+			session.setAttribute("count", cnt + 1);
+
 		}
-		else {
-			
-			//session.isNew();
-			session=request.getSession();
-			
-		}
-		
-		out.print("<h1> Session Id" + session.getId()+"</> <>hr");
-		out.print("<h1> Session Creation Time"+ session.getCreationTime());
-		
-		out.print("<h1> Session Creation Time"+ new Date(session.getCreationTime())+"</h1> <hr>");
-		
-		out.print("<h1> Session LastAcces Time"+new Date(session.getLastAccessedTime())+"</h1> <hr>");
-		out.print("<h1> Session Get Max Time"+session.getMaxInactiveInterval()+"</h1> <hr>");
-		
+		out.print("<h1> Visit Count" + session.getAttribute("count") + "</h1> <hr>");
+		out.print("<h1> Session Id" + session.getId() + "</> <>hr");
+		out.print("<h1> Session Creation Time" + session.getCreationTime());
+
+		out.print("<h1> Session Creation Time" + new Date(session.getCreationTime()) + "</h1> <hr>");
+
+		out.print("<h1> Session LastAcces Time" + new Date(session.getLastAccessedTime()) + "</h1> <hr>");
+		out.print("<h1> Session Get Max Time" + session.getMaxInactiveInterval() + "</h1> <hr>");
+
 		session.setMaxInactiveInterval(60);
-	
-		
+
 	}
 
 }
